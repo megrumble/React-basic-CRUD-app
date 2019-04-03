741618b144a3502b715655eed08b89e5f8a69e5c
unknown <megrumble@gmail.com>
2019-04-03 08:35:24 -0400

    adding client2

---
+++ client2/src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
+
+const App = () => {
+  return (
+    <div>
+      <Router history={history}>
+        <div>
+          <Header className="ui container" />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" component={StreamCreate} />
+            <Route path="/streams/edit/:id" component={StreamEdit} />
+            <Route path="/streams/delete/:id" component={StreamDelete} />
+            <Route path="/streams/:id" component={StreamShow} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
